ba16490b5d6ce22b62f7322aa877f347aff38f11
mharb <mohamed.harb@cegedim.com>
2024-01-02 14:06:24 +0200

    issue in datamatrix generation in latest version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import bwip from 'bwip-js';
 
 import './App.css';
 
-const b64toBlob = (b64Data: string, contentType='', sliceSize=512) => {
+const b64toBlob = (b64Data: string, contentType = '', sliceSize = 512) => {
   const byteCharacters = atob(b64Data);
   const byteArrays = [];
 
@@ -18,30 +18,31 @@ const b64toBlob = (b64Data: string, contentType='', sliceSize=512) => {
     const byteArray = new Uint8Array(byteNumbers);
     byteArrays.push(byteArray);
   }
-    
-  const blob = new Blob(byteArrays, {type: contentType});
+
+  const blob = new Blob(byteArrays, { type: contentType });
   return blob;
 }
 
 function App() {
   const qrCode = 'UEsDBBQACAgIAFJ8n1cAAAAAAAAAAAAAAAAPAAAAcXJHZW5lcmF0b3IueG1sZVHJjptAEL3nK0p1jZduoBuQzCjtBuKWmiUYrMkpYqJJxpItR8P4kL/Pg5lbONTStbxXj11LLs84EEG4lsE6lExHm7HZ27woEdcZSyGEjFKm9ohCoNIoZLJoCnScxKHWyNCWivm5z9hWX5maAYE5rl3JD58I367NCfMCa9BzMHWOqMWDiEQcyDCMdYwFbZHx9XkaX2/Tj6c/X/6OL7fb5uftiooB+tAVnk7eODuQkvR0n0jSSAGsXpHQMlXRik7jdLmfmfrvGRcghQMjnCATmWgVKqXiOFJpwFSCty/qoTdMHvFhOAy+QFL+BxauyL6c386X8f3KrqmYDFR4e70/M1VdxkrKhKlrlitdv7jjI3Ri2oOCTLWeNRbQuJwv5+2HNDk5u3TnGIIIflYRvq8wBbc0LyIj2yP7NV4mgJr5h1TNo7POe1cPFX0GJZcjsN6cBm9qcAdPbE9idf29lQEstCwB0FRtt6HS+WoD5GX3N9hoA6zeLrQ/juhRDRe6u2378A9QSwcIdNgRKYIBAAAzAgAAUEsBAhQAFAAICAgAUnyfV3TYESmCAQAAMwIAAA8AAAAAAAAAAAAAAAAAAAAAAHFyR2VuZXJhdG9yLnhtbFBLBQYAAAAAAQABAD0AAAC/AQAAAAA=';
   const url = URL.createObjectURL(b64toBlob(qrCode, 'application/octet-stream'));
+  const [svg, setSVG] = useState<string>('');
 
   useEffect(() => {
-      try {
-        bwip.toCanvas('qr', {
-          bcid: `datamatrix`,
-          text: atob(qrCode),
-        });
-      } catch (e) {
-        console.error(e);
-      }
+    try {
+      setSVG(bwip.toSVG({
+        bcid: `datamatrix`,
+        text: atob(qrCode),
+      }));
+    } catch (e) {
+      console.error(e);
+    }
   }, []);
 
   return (
-    <div className="App"> 
+    <div className="App">
       <main>
-        <canvas id='qr' className='qr-canvas'/>
+        <div id="qr" dangerouslySetInnerHTML={{ __html: svg }} />
         <div className="spacer"></div>
         <a href={url} download="download.zip">Download</a>
       </main>
